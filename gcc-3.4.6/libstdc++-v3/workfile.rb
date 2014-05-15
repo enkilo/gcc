7b83a2b34e6e933f56dee8308f3710c0b61bdaf9
Roman Senn <roman.l.senn@gmail.com>
2014-05-15 16:00:27 +0200

    ...

---
+++ gcc-3.4.6/libstdc++-v3/workfile.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require File.expand_path(ENV['MOSYNCDIR']+'/rules/mosync_lib.rb')
+
+mod = Module.new
+mod.class_eval do
+	def setup_native
+		@LOCAL_DLLS = ["mosync", "mastd"]
+		if(@GCC_IS_V4 && @GCC_V4_SUB >= 4)
+			@EXTRA_CFLAGS = " -D_STDIO_H"
+		end
+		setup_base
+	end
+	
+	def setup_pipe
+		setup_base
+	end
+	
+	def setup_base
+		if(CONFIG == "")
+			# broken compiler
+			@SPECIFIC_CFLAGS = {"File.c" => " -Wno-unreachable-code"}
+		else
+			@SPECIFIC_CFLAGS = {}
+		end
+		@SOURCES = ["libsupc++","src"]
+		@EXTRA_INCLUDES = [".", "include/mapip", "include", "include/backward", "libsupc++"]
+		@INSTALL_INCDIR = "libstdc++"
+		@NAME = "stdc++"
+	end
+end
+
+MoSyncLib.invoke(mod)
