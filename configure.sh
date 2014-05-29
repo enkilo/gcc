d20c7779bac42989ac75531c378bac45b127edd5
Roman Senn <roman.l.senn@gmail.com>
2014-05-29 13:36:55 +0200

    put build.sh writer into mkbuild.sh

--- configure.sh
+++ configure.sh
@@ -76,7 +76,7 @@ builddir=build/${host}                       # build directory
 #--------------------------------"-------------------------------------------------
 
 mkdir -p $builddir
-cd $builddir
+(cd $builddir
 
 $SRCDIR/configure \
         --enable-languages="c,c++" \
@@ -87,18 +87,6 @@ $SRCDIR/configure \
         --without-headers \
         --program-prefix=$progpref -v \
         2>&1 | tee gcc_configure.log
+)
 
-cat <<EOF| tee build.sh|sed "s/^/build.sh: /"
-
-mkdir -p $builddir/gcc && cp -vf $SRCDIR/gcc/{gengtype-yacc.c,c-parse.c,gengtype-lex.c} $builddir/gcc
-
-make -C $builddir {HOST_,}LDFLAGS="-static" all-gcc
-
-$host-strip -v --strip-all $builddir/gcc/{xgcc,cpp,cc1,cc1plus}*
-
-mkdir -p \${MOSYNCDIR:-$prefix}/{mapip/bin,bin} && 
-cp -vf $builddir/gcc/{xgcc,cpp}* \${MOSYNCDIR:-$prefix}/bin && 
-cp -vf $builddir/gcc/{cc1,cc1plus}* \${MOSYNCDIR:-$prefix}/mapip/bin
-
-#ln -sf xgcc \${MOSYNCDIR:-$prefix}/bin/gcc
-EOF
+. mkbuild.sh
---
+++ mkbuild.sh
@@ -0,0 +1,14 @@
+cat <<EOF| tee build.sh|sed "s/^/build.sh: /"
+
+mkdir -p $builddir/gcc && cp -vf $SRCDIR/gcc/{gengtype-yacc.c,c-parse.c,gengtype-lex.c} $builddir/gcc
+
+make -C $builddir {HOST_,}LDFLAGS="-static" all-gcc
+
+$host-strip -v --strip-all $builddir/gcc/{xgcc,cpp,cc1,cc1plus}*
+
+mkdir -p \${MOSYNCDIR:-$prefix}/{mapip/bin,bin} && 
+cp -vf $builddir/gcc/{xgcc,cpp}* \${MOSYNCDIR:-$prefix}/bin && 
+cp -vf $builddir/gcc/{cc1,cc1plus}* \${MOSYNCDIR:-$prefix}/mapip/bin
+
+#ln -sf xgcc \${MOSYNCDIR:-$prefix}/bin/gcc
+EOF
