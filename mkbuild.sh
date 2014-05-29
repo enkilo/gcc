046a19c04200f3a199eb8ec71732648081a6e63b
Roman Senn <roman.l.senn@gmail.com>
2014-05-29 12:03:40 +0000

    ...

--- mkbuild.sh
+++ mkbuild.sh
@@ -1,16 +1,18 @@
 cat <<EOF| tee build.sh|sed "s/^/build.sh: /"
 
+builddir=\${1-$builddir}
+
 SRCDIR=gcc-3.4.6
 
-mkdir -p $builddir/gcc && cp -vf \$SRCDIR/gcc/{gengtype-yacc.c,c-parse.c,gengtype-lex.c} $builddir/gcc
+mkdir -p \$builddir/gcc && cp -vf \$SRCDIR/gcc/{gengtype-yacc.c,c-parse.c,gengtype-lex.c} \$builddir/gcc
 
-make -C $builddir {HOST_,}LDFLAGS="-static" all-gcc
+make -C \$builddir CFLAGS_FOR_BUILD="-g -Wall" CC_FOR_BUILD=gcc {HOST_,}LDFLAGS="-static" all-gcc
 
-$host-strip -v --strip-all $builddir/gcc/{xgcc,cpp,cc1,cc1plus}*
+$host-strip -v --strip-all \$builddir/gcc/{xgcc,cpp,cc1,cc1plus}*
 
 mkdir -p \${MOSYNCDIR:-$prefix}/{mapip/bin,bin} && 
-cp -vf $builddir/gcc/{xgcc,cpp}* \${MOSYNCDIR:-$prefix}/bin && 
-cp -vf $builddir/gcc/{cc1,cc1plus}* \${MOSYNCDIR:-$prefix}/mapip/bin
+cp -vf \$builddir/gcc/{xgcc,cpp}* \${MOSYNCDIR:-$prefix}/bin && 
+cp -vf \$builddir/gcc/{cc1,cc1plus}* \${MOSYNCDIR:-$prefix}/mapip/bin
 
 #ln -sf xgcc \${MOSYNCDIR:-$prefix}/bin/gcc
 EOF
