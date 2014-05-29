7058b09e020e788bdf11e38e7f80773135384134
Roman Senn <roman.l.senn@gmail.com>
2014-05-29 12:24:00 +0000

    ...

--- configure-linux.sh
+++ configure-linux.sh
@@ -83,10 +83,5 @@ $SRCDIR/configure \
 	--host=$host \
   ) 2>&1 | tee gcc_configure.log
 
-cat <<EOF| tee build.sh|sed "s/^/build.sh: /"
-mkdir -p $builddir/gcc && cp -vf $SRCDIR/gcc/{gengtype-yacc.c,c-parse.c,gengtype-lex.c} $builddir/gcc
-make -C $builddir {HOST_,}LDFLAGS="-static" all-gcc
-strip -v --strip-all $builddir/gcc/{xgcc,cpp,cc1,cc1plus}
-mkdir -p \${MOSYNCDIR:-$prefix}/{mapip/bin,bin} && cp -vf $builddir/gcc/{xgcc,cpp} \${MOSYNCDIR:-$prefix}/bin && cp -vf $builddir/gcc/{cc1,cc1plus} \${MOSYNCDIR:-$prefix}/mapip/bin
-#ln -sf xgcc \${MOSYNCDIR:-$prefix}/bin/gcc
-EOF
+. mkbuild.sh
+
